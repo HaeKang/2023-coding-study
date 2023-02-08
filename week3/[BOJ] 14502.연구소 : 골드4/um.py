bda8ab3b843262a187c027a5acbb4f37066dbb6d
suafion <suafion@gmail.com>
2023-02-08 17:26:06 +0900

    14502fix, 14503add

--- week3/[BOJ] 14502.연구소 : 골드4/um.py
+++ week3/[BOJ] 14502.연구소 : 골드4/um.py
@@ -12,43 +12,39 @@ for y in range(n):
     for x in range(m):
         if dat[y][x] == 0:
             zeros += 1
-print(zeros)
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
 
 def checker(c):
     global max_anjeon, zeros
     if zeros - c < max_anjeon:
+        #print('max anjeon check ', zeros, ' ',c, '  ' ,max_anjeon)
         return False
     return True
 
 def spread_bfs(y,x,mat,cnt):
     global dx, dy, n, m
+    cnt_in = 0
     for kka in range(4):
         ny = y + dy[kka]
         nx = x + dx[kka]
         if 0 <= ny < n and 0 <= nx < m and mat[ny][nx] == 0:
+
             mat[ny][nx] = 2
-            cnt += 1
-            #print('cnt    ',cnt)
-            if checker(cnt):
-                cnt += spread_bfs(ny,nx,mat,cnt)
-    return cnt
+            cnt_in += 1
+            if checker(cnt+cnt_in):
+                cnt_in += spread_bfs(ny,nx,mat,cnt+cnt_in)
+    return cnt_in
 
 def spread(mat):
-    global n,m,dat,max_anjeon
-    for kka in range(n):
-        print(mat[kka])
-    print()
+    global n,m,dat,max_anjeon,zeros
     cnt = 3 # 0->2 갯수 벽포함
     for y in range(n):
         for x in range(m):
             if dat[y][x] == 2:
                 cnt += spread_bfs(y,x,mat,cnt)
-    for kka in range(n):
-        print(mat[kka])
-    asg
-    max_anjeon = max(zeros-cnt,max_anjeon)
+
+    max_anjeon = max(max_anjeon,zeros - cnt)
 
 def mk_buek_bfs(b,bc,mat_w_b):
     global n, m, dat
@@ -56,12 +52,13 @@ def mk_buek_bfs(b,bc,mat_w_b):
         for y in range(n):
             for x in range(m):
                 if bc[y][x] == False and dat[y][x] == 0:
-                    mat_w_b[y][x] = 1
+                    bc[y][x] = True
                     bc_t = copy.deepcopy(bc)
-                    bc_t[y][x] = True
                     mat = copy.deepcopy(mat_w_b)
+                    mat[y][x] = 1
                     mk_buek_bfs(b-1,bc_t,mat)
-    spread(mat_w_b)
+    else:
+        spread(mat_w_b)
 
 def run():
     global n,m,dat
